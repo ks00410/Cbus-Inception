3f590a4660251293949ff9741d9112ac0f6ef12f
smithkyle1992 <64297775+smithkyle1992@users.noreply.github.com>
2026-09-03 09:25:42 +1000

    Update Resident-LongPoll.lua to current working version
    - Replace hardcoded credentials with secrets library
    - Use inception module for API calls and bitmask decoders (inception.Inception_Post, inception.areaeval, etc.)
    - Add input monitoring block (CBUS-Input-Monitor) with zone loop
    - Add tsuinputs initialisation
    - Fix HTTP timeout: 61s (was 30s, too short for 60s long-poll)
    - Fix empty body handling: silent return instead of logging as error
    - Remove whichid() helper (unused)
    - Remove duplicated local areaeval/dooreval (now in inception module)
    - Comment out SetCBusLabel call (inactive)

--- Resident-LongPoll.lua
+++ Resident-LongPoll.lua
@@ -1,22 +1,28 @@
+-- require("user.inception")
+-- alarm.Resident_Poll()
+
 -- Inner Range Inception Script for Long Polling Status --
 
+-- ToDo:
+-- Implement a nicer way to manage the area ids and mapping to user parameters (and subsequently updating the user parameters)
+-- separate out the functions for querying the API
+
+-- need some logic to rate limit the script incase the long poll is unavaliable
+
 ------------------------------
-     -- User Settings --
+     -- User Scripts --
+------------------------------
+
+require("user.inception")
+require("user.secrets")
+
+------------------------------
+     -- User Secrets --
 ------------------------------
 
 -- Inception URL and Credentials
-local api_token = "#"
-local API_ROOT = "http://192.168.1.112/api/v1"
-
--- Inception IDs for querying APIs
-local area_id = "##"
-local front_door_id = "##"
-local rear_door_id = "##"
-
--- NAC User Parameters
-local CBUS_USERPARAM_NAME_ALARMSTATE = "alarmstate"
-local CBUS_USERPARAM_NAME_FRONTDOOR = "frontdoor"
-local CBUS_USERPARAM_NAME_REARDOOR = "reardoor"
+local api_token = secrets.api_token
+local API_ROOT = secrets.API_ROOT
 
 ------------------------------
        -- Utilities --
@@ -35,113 +41,8 @@ end
 if isempty(tsudoors) then
   tsudoors = "0"
 end
-
--- function to return the correct door/area to update
-local function whichid(id)
-if id == area_id then result = CBUS_USERPARAM_NAME_ALARMSTATE
-elseif id == front_door_id then result = CBUS_USERPARAM_NAME_FRONTDOOR
-elseif id == rear_door_id then result = CBUS_USERPARAM_NAME_REARDOOR
-else result = "no"
-end
-return result
-end
-
-------------------------------
--- Inception Array Decoding --
-------------------------------
-
--- Area Decode Function
-local function areaeval(res)
---log("Number coming in is... " .. res)
-string = ""
-
-vals = {
-'Armed',
-'Alarm',
-'Entry Delay',
-'Exit Delay',
-'Arm Warning',
-'Defer Disarmed',
-'Detecting Active Inputs',
-'Walk Test Active',
-'Away Arm',
-'Stay Arm',
-'Sleep Arm',
-' Disarmed',
-'Arm Ready'
-}
-
-s = ''
-for i=0,#vals-1 do
-  if bit.band(res, 2^i) > 0 then
-    s = (#s>0 and s..' - '..vals[i+1]) or vals[i+1]
-  end
-end
---log("string going out is... ".. string)
-return(s)
-end	
-	
--- Door Decode Function
-local function dooreval(res)
-vals = {
-'Unlocked',
-'Open',
-'Locked Out',
-'Forced',
-'Held Open Warning',
-'Held Open Too Long',
-'Breakglass',
-'Reader Tamper',
-'Locked',
-'Closed',
-'Held Response Muted'
-}
-
-s = ''
-for i=0,#vals-1 do
-  if bit.band(res, 2^i) > 0 then
-    s = (#s>0 and s..' - '..vals[i+1]) or vals[i+1]
-  end
-end
---log("string going out is... ".. string)
-return(s)
-end	
-
-------------------------------
-    -- C-Bus Functions --
-------------------------------
-
-
-------------------------------
---  INCEPTION API Functions --
-------------------------------
-
-local function inceptionapi(payload,endpoint)
-local http = require("socket.http")
-local json = require("json")
-	
-	local body, code, headers, status = http.request {
-	method = "POST",
-	url = API_ROOT .. "/" .. endpoint,
-	headers  =
-			{
-			["Accept"] = "application/json";
-			["Authorization"] = "APIToken " .. api_token;
-			},
-	body = payload,
-	timeout = 30,
-	}
---log("Getting Data from Endpoint URL " .. API_ROOT .. '/' .. endpoint .. '\n' .. "tsuarea is " ..tsuarea .. '\n' .."TSUdoors is " .. tsudoors .. '\n'.. 'body:' .. tostring(body) .. '\n' .. 'code:' .. tostring(code) .. '\n' .. 'status:' .. tostring(status))
-
-if (code ~= 200) then
-    log("Inception post error : "..tostring(code)..","..tostring(body))
-    return
-elseif isempty(body) then
-    log("Inception post error : "..tostring(code)..","..tostring(body))
-    return
-else return body
-
-end
+if isempty(tsuinputs) then
+  tsuinputs = "0"
 end
 
 ------------------------------
@@ -165,39 +66,56 @@ local payload = json.encode(
 		stateType = "DoorState",
 		timeSinceUpdate = tsudoors
 	}
+},
+{
+	ID = "CBUS-Input-Monitor",
+	RequestType = "MonitorEntityStates",
+	InputData = {
+		stateType = "InputState",
+		timeSinceUpdate = tsuinputs
+	}
 }
 })	
+
+-- log("Area: " .. tsuarea .. "Doors: " .. tsudoors .. "Inputs: " .. tsuinputs)
 
 ------------------------------
 	--  Resident Code  --
 ------------------------------
+--log("alarm script is running")
 
 -- Query the API
-local body = inceptionapi(payload,"monitor-updates")
+local body = inception.Inception_Post(payload,"monitor-updates")
+--  log(payload)
+--	log(body)
 
--- Process the Response
--- NOTE: This needs cleaning up.
-if body then
+-- Process the Response TODO: Clean this Up!
+
+if isempty(body) == false then
   local httptable = json.pdecode(body)
-  -- log(httptable)
+
   -- check if the response was related to areas?
   if tostring(httptable["ID"]) == "CBUS-Areas-Monitor" then
 	tsuarea = tostring(httptable["Result"]["updateTime"])
 	-- Find the matching area ID and translate it to a string
 	local numUpdates = table.getn(httptable["Result"]["stateData"])
-	for httptableindex = 1,numUpdates,1
+--	  log("Number of Updates: " .. numUpdates)
+--    log(httptable)
+
+    for httptableindex = 1,numUpdates,1
 	do
-	  if httptable["Result"]["stateData"][httptableindex]["ID"] == area_id then
+	  if httptable["Result"]["stateData"][httptableindex]["ID"] == secrets.area_id then
 		local PublicState = httptable["Result"]["stateData"][httptableindex]["PublicState"]
-		local AlarmAreaStatus = areaeval(PublicState)
-		log("Alarm Area Status is: " .. AlarmAreaStatus .. " which was calculated from a Public State ID of " .. PublicState)						
+		local AlarmAreaStatus = inception.areaeval(PublicState)
+--		log("Alarm Area Status is: " .. AlarmAreaStatus .. " which was calculated from a Public State ID of " .. PublicState)						
 		  -- Set the C-Bus User Parameter
-		SetUserParam(0, CBUS_USERPARAM_NAME_ALARMSTATE, AlarmAreaStatus)
-		  --	log("We have a match! " .. tostring(httptable["Result"]["stateData"][httptableindex]["ID"]) .. " The Status is now set to " .. tostring(httptable["Result"]["stateData"][httptableindex]["PublicState"]))
+		SetUserParam(0, inception.CBUS_USERPARAM_NAME_ALARMSTATE, AlarmAreaStatus)
+--		  	log("We have a match! " .. tostring(httptable["Result"]["stateData"][httptableindex]["ID"]) .. " The Status is now set to " .. tostring(httptable["Result"]["stateData"][httptableindex]["PublicState"]))
 	  end
 	end
 end
-    -- check if the response was related to doors?
+
+  -- check if the response was related to doors?
 
 if tostring(httptable["ID"]) == "CBUS-Doors-Monitor" then
 	tsudoors = tostring(httptable["Result"]["updateTime"])    
@@ -205,26 +123,62 @@ if tostring(httptable["ID"]) == "CBUS-Doors-Monitor" then
 	local numUpdates = table.getn(httptable["Result"]["stateData"])
 	for httptableindex = 1,numUpdates,1
 	do
-		if httptable["Result"]["stateData"][httptableindex]["ID"] == front_door_id then
+		if httptable["Result"]["stateData"][httptableindex]["ID"] == secrets.front_door_id then
 			local PublicState = httptable["Result"]["stateData"][httptableindex]["PublicState"]
-			local FrontDoorStatus = dooreval(PublicState)
-			log("Front Door Status is: " .. FrontDoorStatus .. " which was calculated from a Public State ID of " .. PublicState)						
+			local FrontDoorStatus = inception.dooreval(PublicState)
+--			log("Front Door Status is: " .. FrontDoorStatus .. " which was calculated from a Public State ID of " .. PublicState)						
 		-- Set the C-Bus User Parameter
-	SetUserParam(0, CBUS_USERPARAM_NAME_FRONTDOOR, FrontDoorStatus)
+	SetUserParam(0, inception.CBUS_USERPARAM_NAME_FRONTDOOR, FrontDoorStatus)
 	local strn = string.split(FrontDoorStatus, "-")
 	local DoorScreenLabel = tostring(strn[1])
-	SetCBusLabel(0,56,122,1,'Variant 1',string.sub(DoorScreenLabel, 1, 13))
-	log("Setting C-Bus Label to: " .. tostring(DoorScreenLabel))
+	-- SetCBusLabel(0,56,122,1,'Variant 1',string.sub(DoorScreenLabel, 1, 13))
+--	log("Setting C-Bus Label to: " .. tostring(DoorScreenLabel))
 	--	log("We have a match! " .. tostring(httptable["Result"]["stateData"][httptableindex]["ID"]) .. " The Status is now set to " .. tostring(httptable["Result"]["stateData"][httptableindex]["PublicState"]))
 		end
-		if httptable["Result"]["stateData"][httptableindex]["ID"] == rear_door_id then
+		if httptable["Result"]["stateData"][httptableindex]["ID"] == secrets.rear_door_id then
 			local PublicState = httptable["Result"]["stateData"][httptableindex]["PublicState"]
-			local RearDoorStatus = dooreval(PublicState)
-			log("Rear Door Status is: " .. RearDoorStatus .. " which was calculated from a Public State ID of " .. PublicState)						
+			local RearDoorStatus = inception.dooreval(PublicState)
+--			log("Rear Door Status is: " .. RearDoorStatus .. " which was calculated from a Public State ID of " .. PublicState)						
 		-- Set the C-Bus User Parameter
-			SetUserParam(0, CBUS_USERPARAM_NAME_REARDOOR, RearDoorStatus)
+			SetUserParam(0, inception.CBUS_USERPARAM_NAME_REARDOOR, RearDoorStatus)
 		--	log("We have a match! " .. tostring(httptable["Result"]["stateData"][httptableindex]["ID"]) .. " The Status is now set to " .. tostring(httptable["Result"]["stateData"][httptableindex]["PublicState"]))
 		end
+	end
 end
+
+-- COMING SOON, INPUT MONITORING --
+
+  -- check if the response was related to inputs?
+  if tostring(httptable["ID"]) == "CBUS-Input-Monitor" then
+   --log("Look, We found some inputs!")
+   -- log(httptable)
+	tsuinputs = tostring(httptable["Result"]["updateTime"])
+	--build a table of zones we are interested in (todo define the secrets as a table instead)
+  local zones = {secrets.zone1, secrets.zone2, secrets.zone3, secrets.zone4, secrets.zone5, secrets.zone6, secrets.zone7, secrets.zone8}
+  local numZones = table.getn(zones)
+  --log(numZones)
+   
+  -- Find the matching area ID and translate it to a string
+	local numUpdates = table.getn(httptable["Result"]["stateData"])
+	  --log("Number of Updates: " .. numUpdates)
+    --log(httptable)
+
+    for httptableindex = 1,numUpdates,1
+    do
+      for zonesindex = 1,numZones,1
+      do
+      -- log("Reviewing Result: " .. httptableindex .. " against zone id: " .. zonesindex .. " API Result is: " .. httptable["Result"]["stateData"][httptableindex]["ID"] .. " Zone ID is: " .. zones[zonesindex] .. " Is it a match? ")
+        if httptable["Result"]["stateData"][httptableindex]["ID"] == zones[zonesindex] then
+        local PublicState = httptable["Result"]["stateData"][httptableindex]["PublicState"]
+        local AlarmInputStatus = inception.inputeval(PublicState)
+--        log("zone" .. zonesindex .. " Status is: " .. AlarmInputStatus .. " which was calculated from a Public State ID of " .. PublicState)						
+   			-- log("user param: " .. inception.CBUS_USERPARAM_NAME_ZONES[zonesindex] .. "status: " .. AlarmInputStatus)
+          --		  -- Set the C-Bus User Parameter
+    SetUserParam(0, inception.CBUS_USERPARAM_NAME_ZONES[zonesindex], AlarmInputStatus)
+    --		  --	log("We have a match! " .. tostring(httptable["Result"]["stateData"][httptableindex]["ID"]) .. " The Status is now set to " .. tostring(httptable["Result"]["stateData"][httptableindex]["PublicState"]))
+      	end
+      end
+    end
 end
+
 end
